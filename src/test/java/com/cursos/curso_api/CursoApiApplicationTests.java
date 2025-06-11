4ae9a813adaec6895d5c6b96b68fb085ec881731
glaubersantos <glauber.dmi@gmail.com>
2025-06-11 07:40:02 -0300

    Build bem-sucedido com teste passando

--- src/test/java/com/cursos/curso_api/CursoApiApplicationTests.java
+++ src/test/java/com/cursos/curso_api/CursoApiApplicationTests.java
@@ -2,12 +2,18 @@ package com.cursos.curso_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=update"
+})
 class CursoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
